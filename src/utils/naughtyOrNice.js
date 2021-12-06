378740068b6b7b8ca9db47fa5907a1052a499141
Marc Backes <hello@marc.dev>
2021-12-06 13:56:58 +0100

    initial commit 📜

---
+++ src/utils/naughtyOrNice.js
@@ -0,0 +1,14 @@
+// Check the instructions in the README.md file
+
+export const findOutIfNaughtyOrNice = kid => {
+    return ''
+}
+export const getKids = () => {
+    return []
+}
+export const getNiceKids = kids => {
+    return []
+}
+export const getNaughtyKids = kids => {
+    return []
+}
